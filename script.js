96599a47be213fb1d75132684611554c523a994c
swathikanagaraj001-maker <swathikanagaraj001@gmail.com>
2026-06-03 20:07:57 +0530

    Updated  Portfolio Project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hire Me Button
+function showMessage(){
+  alert("Thank you for visiting my portfolio!");
+}
+
+// Dark Mode
+function toggleDarkMode(){
+  document.body.classList.toggle("dark-mode");
+}
